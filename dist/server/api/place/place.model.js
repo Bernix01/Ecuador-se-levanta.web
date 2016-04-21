12df88b189325d0927d8cbc89c68bbd4c129982c
Guillermo Bernal <gbernal096@gmail.com>
2016-04-21 10:13:57 -0500

    ...

--- dist/server/api/place/place.model.js
+++ dist/server/api/place/place.model.js
@@ -14,19 +14,8 @@ var PlaceSchema = new _mongoose2.default.Schema({
   name: String,
   lat: { type: Number, default: 0 },
   long: { type: Number, default: 0 },
-  needs: [{
-    name: String,
-    image: String,
-    detail: { type: String, default: '' },
-    state: Boolean
-  }],
-  wtgo: [{
-    name: String,
-    tel: { type: String, default: '' },
-    extra: { type: String, default: '' },
-    lat: { type: Number, default: 0 },
-    long: { type: Number, default: 0 }
-  }],
+  needs: [],
+  wtgo: [],
   htgo: { type: String, default: '' }
 });
 
--- server/api/place/place.model.js
+++ server/api/place/place.model.js
@@ -6,19 +6,8 @@ var PlaceSchema = new mongoose.Schema({
   name: String,
   lat: { type: Number, default: 0 },
   long: { type: Number, default: 0 },
-  needs: [{
-    name:String,
-    image: String,
-    detail: { type: String, default: '' },
-    state: Boolean
-  }],
-  wtgo:[{
-    name: String,
-    tel: { type: String, default: '' },
-    extra: { type: String, default: '' },
-    lat: { type: Number, default: 0 },
-    long: { type: Number, default: 0 }
-  }],
+  needs: [],
+  wtgo:[],
   htgo: { type: String, default: '' }
 });
 
