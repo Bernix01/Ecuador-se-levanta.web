8097029c908cae5ad0cffaccd7c4061d9e8d3137
Guillermo Bernal <gbernal096@gmail.com>
2016-05-14 19:19:37 -0500

    prod email

--- dist/server/config/environment/production.js
+++ dist/server/config/environment/production.js
@@ -4,15 +4,18 @@
 // =================================
 
 module.exports = {
-        // Server IP
-        ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
+  // Server IP
+  ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
 
-        // Server port
-        port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
+  // Server port
+  port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
 
-        // MongoDB connection options
-        mongo: {
-                uri: process.env.DB_URI
-        }
+  // MongoDB connection options
+  mongo: {
+    uri: process.env.DB_URI
+  },
+
+  // Seed database on startup
+  seedDB: true
 };
 //# sourceMappingURL=production.js.map
